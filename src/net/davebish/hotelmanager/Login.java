9edabef918409d01799ca9d9fbde9c5a4eb57438
davebish <david.b.bishop@gmail.com>
2014-07-12 17:28:22 -0700

    Hotel App with WindowsBuilder - initial version

---
+++ src/net/davebish/hotelmanager/Login.java
@@ -0,0 +1,123 @@
+package net.davebish.hotelmanager;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.KeyStroke;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+@SuppressWarnings("serial")
+public class Login extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtUserName;
+	private JPasswordField pwdPassword;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Login() {
+		setTitle("Login");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(5, 5, 440, 10);
+		contentPane.add(panel);
+		
+		JLabel lblUserName = new JLabel("User Name:");
+		lblUserName.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblUserName.setBounds(102, 88, 91, 15);
+		contentPane.add(lblUserName);
+		
+		JLabel lblPassword = new JLabel("Password:");
+		lblPassword.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblPassword.setBounds(102, 133, 91, 15);
+		contentPane.add(lblPassword);
+		
+		txtUserName = new JTextField();
+		txtUserName.setBounds(211, 86, 168, 19);
+		txtUserName.setColumns(20);
+		contentPane.add(txtUserName);
+
+		
+		pwdPassword = new JPasswordField();
+		pwdPassword.setColumns(20);
+		pwdPassword.setBounds(211, 131, 168, 19);
+		contentPane.add(pwdPassword);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.addActionListener(new btnLoginListener());
+		btnLogin.setBounds(262, 221, 117, 25);
+		contentPane.add(btnLogin);
+		// Make the Login button the default if the enter key is pressed
+		getRootPane().setDefaultButton(btnLogin);
+		
+		JButton btnCancel = new JButton("Cancel");
+		contentPane.add(btnCancel);
+		btnCancel.addActionListener(new btnCancelListener());
+		btnCancel.setBounds(120, 221, 117, 25);
+		// Make the escape key close the window
+		KeyStroke escKey = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0, false);
+		Action escapeAction = new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				Login.this.dispose();
+			}
+		};
+		getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(escKey,"ESCAPE");
+		getRootPane().getActionMap().put("ESCAPE", escapeAction);
+		// Put focus on txtUserName
+		txtUserName.requestFocus();
+		setLocationRelativeTo(null);
+	}
+
+	private class btnCancelListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			// do something in response to cancel clicked
+			Login.this.dispose();
+		}
+		
+	}
+
+	private class btnLoginListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			// do something in response to login clicked
+			JOptionPane.showMessageDialog(Login.this, "Attempting login. \nUserName="+txtUserName.getText()+
+                    "\nPassword=" + new String(pwdPassword.getPassword()));
+		}
+	}
+}
