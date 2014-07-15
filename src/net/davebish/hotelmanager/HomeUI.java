6acf44f6bafc424fba38d3801cd0b13b1ee9a6e3
davebish <david.b.bishop@gmail.com>
2014-07-15 10:09:21 -0700

    reservatino menu created

--- src/net/davebish/hotelmanager/HomeUI.java
+++ src/net/davebish/hotelmanager/HomeUI.java
@@ -4,8 +4,13 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import java.awt.event.*;
 
 public class HomeUI extends JFrame {
 
@@ -20,7 +25,45 @@ public class HomeUI extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
+		setupMenu();		
 		setContentPane(contentPane);
 	}
 
+	private void setupMenu() {
+		JMenu reservation = new JMenu("Reservations");
+		reservation.setMnemonic('R');
+		
+		JMenuItem newItem = new JMenuItem("New");
+		newItem.setMnemonic('N');
+		reservation.add(newItem);
+		newItem.addActionListener(
+				new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						// handle the new reservation request
+					}
+				});
+		
+		JMenuItem modifyItem = new JMenuItem("Modify");
+		modifyItem.setMnemonic('M');
+		reservation.add(modifyItem);
+		modifyItem.addActionListener(
+				new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						// handle the modify reservation request
+					}
+				});		
+		
+		JMenuItem cancelItem = new JMenuItem("Cancel");
+		cancelItem.setMnemonic('C');
+		reservation.add(cancelItem);		
+		cancelItem.addActionListener(
+				new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						// handle the cancel reservation request
+					}
+				});	
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		menuBar.add(reservation);
+	}
 }
